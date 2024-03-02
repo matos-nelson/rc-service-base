0d5721dd77d9d485a97c5b244a105b7b3e3d71e7
matos-nelson <nmatos6@gatech.edu>
2024-03-02 14:40:42 -0600

    Added base model to service base.

---
+++ runtime/src/main/java/rent/circle/rc/service/base/runtime/persistence/BaseModel.java
@@ -0,0 +1,19 @@
+package rent.circle.rc.service.base.runtime.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@MappedSuperclass
+public abstract class BaseModel {
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
